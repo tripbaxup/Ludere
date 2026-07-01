c23b65ebb36edfa1b91678aaf4b524c0e77e3c52
triptrap-lsd <inbloomapplicants@gmail.com>
2026-07-01 07:30:51 -0400

    Revert GamePad.kt to use RadialGamePad

--- app/src/main/java/com/draco/ludere/gamepad/GamePad.kt
+++ app/src/main/java/com/draco/ludere/gamepad/GamePad.kt
@@ -13,16 +13,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.draco.ludere.R
 import com.draco.ludere.input.N64InputHandler
 import com.swordfish.libretrodroid.GLRetroView
-import io.github.controlwear.virtual.joystick.android.JoystickView
+import com.swordfish.radialgamepad.library.RadialGamePad
+import com.swordfish.radialgamepad.library.config.RadialGamePadConfig
+import com.swordfish.radialgamepad.library.event.Event
 import io.reactivex.disposables.CompositeDisposable
-import kotlin.math.cos
-import kotlin.math.sin
 
 class GamePad(
     context: Context,
+    padConfig: RadialGamePadConfig,
     private val sharedN64Handler: N64InputHandler? = null,
 ) {
-    val pad = JoystickView(context)
+    val pad = RadialGamePad(padConfig, 0f, context)
 
     companion object {
         @Suppress("DEPRECATION")
@@ -56,31 +57,37 @@ class GamePad(
         }
     }
 
-    fun subscribe(compositeDisposable: CompositeDisposable, retroView: GLRetroView) {
-        pad.setOnMoveListener { angle, strength ->
-            val deadZone = 10
-            if (strength < deadZone) {
-                if (sharedN64Handler != null && !sharedN64Handler.useAnalogStick) {
-                    retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_DPAD, 0f, 0f)
-                } else if (sharedN64Handler == null) {
-                    retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_DPAD, 0f, 0f)
-                } else {
-                    retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_ANALOG_LEFT, 0f, 0f)
+    private fun eventHandler(event: Event, retroView: GLRetroView) {
+        when (event) {
+            is Event.Button -> retroView.sendKeyEvent(event.action, event.id)
+            is Event.Direction -> {
+                when (event.id) {
+                    GLRetroView.MOTION_SOURCE_DPAD -> {
+                        if (sharedN64Handler != null && !sharedN64Handler.useAnalogStick) {
+                            retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_DPAD, event.xAxis, event.yAxis)
+                        } else if (sharedN64Handler == null) {
+                            retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_DPAD, event.xAxis, event.yAxis)
+                        }
+                    }
+                    GLRetroView.MOTION_SOURCE_ANALOG_LEFT -> {
+                        if (sharedN64Handler != null && sharedN64Handler.useAnalogStick) {
+                            retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_ANALOG_LEFT, event.xAxis, event.yAxis)
+                        } else if (sharedN64Handler == null) {
+                            retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_ANALOG_LEFT, event.xAxis, event.yAxis)
+                        }
+                    }
+                    GLRetroView.MOTION_SOURCE_ANALOG_RIGHT -> {
+                        retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_ANALOG_RIGHT, event.xAxis, event.yAxis)
+                    }
                 }
-                return@setOnMoveListener
-            }
-
-            val rad = Math.toRadians(angle.toDouble())
-            val x = (cos(rad) * (strength / 100.0)).toFloat()
-            val y = (sin(rad) * (strength / 100.0)).toFloat()
-
-            if (sharedN64Handler != null && !sharedN64Handler.useAnalogStick) {
-                retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_DPAD, x, y)
-            } else if (sharedN64Handler == null) {
-                retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_ANALOG_LEFT, x, y)
-            } else {
-                retroView.sendMotionEvent(GLRetroView.MOTION_SOURCE_ANALOG_LEFT, x, y)
             }
         }
+    }
+
+    fun subscribe(compositeDisposable: CompositeDisposable, retroView: GLRetroView) {
+        val inputDisposable = pad.events().subscribe {
+            eventHandler(it, retroView)
+        }
+        compositeDisposable.add(inputDisposable)
     }
 }
